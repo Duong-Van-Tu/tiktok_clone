65f126d23ced70b1f47881271700fffcbea5a29a
Duong-Van-Tu <79387859+Duong-Van-Tu@users.noreply.github.com>
2023-10-10 17:18:42 +0700

    Update MainLayout.tsx

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -11,7 +11,7 @@ function MainLayout({ children }: { children: React.ReactNode }) {
       <div
         className={[
           useLocation().pathname === "/" ? "max-w-[1140px]" : "",
-          "flex justify-between mx-auto w-full lg:px-2.5 px-0",
+          "mx-auto w-full lg:px-2.5 px-0",
         ].join(" ")}
       >
         <div>
